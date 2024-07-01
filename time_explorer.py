54d96e8df4bb9ebb605af55db854b1188accbd63
Kan <90940106+Im-Kan@users.noreply.github.com>
2024-07-01 11:33:56 -0300

    busca dinamica das colunas

--- time_explorer.py
+++ time_explorer.py
@@ -1,4 +1,4 @@
-
+import consts
 
 list_not_fit = [
     'Work Item does not exist, or you do not have permissions to read it',
@@ -10,13 +10,15 @@ list_not_fit = [
 def get_calendar_hashmap(ws):
     calendar_hashmap = {}
     n = 5
+    date_letter = get_column_letter(ws,'Date')
+    title_letter = get_column_letter(ws,'Title')
     while True:
         if ws['A'+n.__str__()].value is None:
             break
-        date_cell = ws['A'+n.__str__()].value.__str__()[0:10].split('-')
+        date_cell = ws[date_letter+n.__str__()].value.__str__()[0:10].split('-')
         data = int(trim(date_cell[2]))
 
-        title_cell = ws['B'+n.__str__()].value
+        title_cell = ws[title_letter+n.__str__()].value
         if title_cell.__str__() in list_not_fit:
             n = n+1
             continue
@@ -24,7 +26,7 @@ def get_calendar_hashmap(ws):
         if cur is None:
             calendar_hashmap[data] = title_cell
         else:
-            calendar_hashmap[data] = cur + " | " + title_cell
+            calendar_hashmap[data] = str(cur) + " | " + str(title_cell)
         n = n+1
     return calendar_hashmap
 
@@ -34,3 +36,12 @@ def trim(string):
         return string[1:]
     else:
         return string
+
+def get_column_letter(ws,name):
+    row = 4
+    letter = ''
+    for l in consts.letters:
+        if ws[l+str(row)].value == name:
+            letter = l
+            break
+    return letter
